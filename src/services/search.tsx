b98553f820a69b96654e0e1cf7aa19888befcbb0
sling62 <samanthaling08@hotmail.co.uk>
2024-01-24 21:32:01 +0000

    refactor: move fetch for search results outside of search-results component

---
+++ src/services/search.tsx
@@ -0,0 +1,34 @@
+import { Rooms } from "@/utils/composition.service";
+
+export async function getData(params: {
+  [key: string]: string | string[] | undefined;
+}) {
+  const body = {
+    bookingType: params.bookingType,
+    direct: false,
+    location: params.location,
+    departureDate: params.departureDate,
+    duration: params.duration,
+    gateway: params.gateway,
+    partyCompositions: Rooms.parseAndConvert([
+      params.partyCompositions as string,
+    ]),
+  };
+
+  const res = await fetch(
+    "https://www.virginholidays.co.uk/cjs-search-api/search",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
