4bb44d10c873ec80b267f6d5ff5042f0400574bc
sling62 <samanthaling08@hotmail.co.uk>
2024-01-25 11:27:18 +0000

    refactor: delete filters component and move back to search-results component

--- src/app/components/molecules/search-results/index.tsx
+++ src/app/components/molecules/search-results/index.tsx
@@ -1,5 +1,16 @@
+"use client";
 import { Holiday } from "@/types/booking";
-import { Filters } from "../filters/Filters";
+import {
+  hotelFacilitiesFilters,
+  pricePerPersonFilters,
+  starRatingFilters,
+} from "@/utils/constants";
+import { useState } from "react";
+import { getHolidaysFilteredByHotelFacilities } from "@/utils/filters/hotelFacilities";
+import { getHolidaysFilteredByStarRating } from "@/utils/filters/starRating";
+import { getHolidaysFilteredByPricePerPerson } from "@/utils/filters/pricePerPerson";
+import { getCommonHotelIdsFromAllFilterResults } from "@/utils/filters";
+import { Checkbox } from "../../atoms/Checkbox";
 
 interface SearchResultsComponentProps {
   holidayResults: Holiday[];
@@ -8,36 +19,62 @@ interface SearchResultsComponentProps {
 export const SearchResultsComponent = ({
   holidayResults,
 }: SearchResultsComponentProps) => {
-  // const holidaysFromEachFilter = [
-  //   getHolidaysFilteredByHotelFacilities(
-  //     holidayResults,
-  //     hotelFacilityFiltersSelected
-  //   ),
-  //   getHolidaysFilteredByStarRating(
-  //     holidayResults,
-  //     starRatingFiltersSelected
-  //   ),
-  //   getHolidaysFilteredByPricePerPerson(
-  //     holidayResults,
-  //     pricePerPersonFiltersSelected
-  //   ),
-  // ].filter((holidays) => holidays.length !== 0);
+  const [hotelFacilityFiltersSelected, setHotelFacilityFiltersSelected] =
+    useState(hotelFacilitiesFilters);
+  const [starRatingFiltersSelected, setStarRatingFiltersSelected] =
+    useState(starRatingFilters);
+  const [pricePerPersonFiltersSelected, setPricePerPersonFiltersSelected] =
+    useState(pricePerPersonFilters);
 
-  // const hotelIds = getCommonHotelIdsFromAllFilterResults(
-  //   holidaysFromEachFilter
-  // );
+  const holidaysFromEachFilter = [
+    getHolidaysFilteredByHotelFacilities(
+      holidayResults,
+      hotelFacilityFiltersSelected
+    ),
+    getHolidaysFilteredByStarRating(holidayResults, starRatingFiltersSelected),
+    getHolidaysFilteredByPricePerPerson(
+      holidayResults,
+      pricePerPersonFiltersSelected
+    ),
+  ].filter((holidays) => holidays.length !== 0);
 
-  // const filteredResults = holidayResults.filter((holiday) =>
-  //   hotelIds.includes(holiday.hotel.id)
-  // );
+  const hotelIds = getCommonHotelIdsFromAllFilterResults(
+    holidaysFromEachFilter
+  );
 
-  // console.log(filteredResults.length);
+  const filteredResults = holidayResults.filter((holiday) =>
+    hotelIds.includes(holiday.hotel.id)
+  );
+
+  console.log(filteredResults.length);
+
+  const onHotelFacilityFilterClick = (filterName: string) => {
+    let currentSelectedFilters = hotelFacilityFiltersSelected;
+    const indexOfClickedFilter = currentSelectedFilters.findIndex(
+      (facility) => facility.name === filterName
+    );
+    currentSelectedFilters[indexOfClickedFilter].selected =
+      !currentSelectedFilters[indexOfClickedFilter].selected;
+    setHotelFacilityFiltersSelected(currentSelectedFilters);
+
+    console.log(hotelFacilityFiltersSelected);
+  };
 
   return (
     <section>
       <h2>{holidayResults?.length} results found</h2>
       <p>Please fill out the filters and results list below&hellip;</p>
-      <Filters holidayResults={holidayResults} />
+      {hotelFacilitiesFilters.map((hotelFacilityFilter) => {
+        const hotelFacilityName = hotelFacilityFilter.name;
+        return (
+          <Checkbox
+            key={hotelFacilityName}
+            label={hotelFacilityName}
+            checked={false}
+            onClick={() => onHotelFacilityFilterClick(hotelFacilityName)}
+          />
+        );
+      })}
     </section>
   );
 };
